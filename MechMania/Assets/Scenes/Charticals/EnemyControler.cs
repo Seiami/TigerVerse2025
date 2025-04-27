2d1deabb0e46a95f162b0eecd6b8ac465bb98753
Charles Bacher <teabeignet@gmail.com>
2025-04-27 05:26:38 -0500

    drone fuller functionality

--- MechMania/Assets/Scenes/Charticals/EnemyControler.cs
+++ MechMania/Assets/Scenes/Charticals/EnemyControler.cs
@@ -6,21 +6,23 @@ public class EnemyControler : MonoBehaviour
 {
     // Variables
     float speed = 7.5f;
-    // float dydz;
+    float dydz;
+    float dxdz;
 
     // Start is called before the first frame update
     void Start()
     {
         Vector3 peepee = new Vector3(0f,6.26f,-33.12f);
         Vector3 difs = transform.position - peepee;
-        // dydz = difs[1];
+        dydz = difs[1]/difs[2];
+        dxdz = difs[0]/difs[2];
         transform.eulerAngles = new Vector3(Mathf.Atan(difs[1]/difs[2])*(180/3.14f)-2,Mathf.Atan(difs[0]/difs[2])*(180/3.14f),0f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += new Vector3(-0.17f*(Mathf.Abs(transform.position[0])/transform.position[0]) ,0f,-1f)*Time.deltaTime*speed;
+        transform.position += new Vector3(-dxdz ,-dydz, -1f)*Time.deltaTime*speed;
         if(transform.position[2]<-36) {
             Destroy(gameObject);
         }
