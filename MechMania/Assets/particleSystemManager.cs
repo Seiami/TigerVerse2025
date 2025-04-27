557f0bf4f615289a2da8042afe96d7f722f40992
Charles Bacher <teabeignet@gmail.com>
2025-04-27 09:18:39 -0500

    idk, max did some stuff

---
+++ MechMania/Assets/particleSystemManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class particleSystemManager : MonoBehaviour
+{
+    public float particleLifetime = 3f;
+    // on failure to hit notes
+    // reduce particle lifetime by .5
+    
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
