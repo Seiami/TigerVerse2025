5e93305f838b24fa3eb29223f1708145713c9b68
Raquel Fellman <115256137+Seiami@users.noreply.github.com>
2025-05-24 15:11:19 -0500

    Added menu rough draft and messing music/beat game desync

---
+++ MechMania/Assets/Scripts/BeatS/PulseToTheBeat.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//https://www.youtube.com/watch?v=gIjajeyjRfE
+public class PulseToTheBeat : MonoBehaviour
+{
+
+    [SerializeField] bool _useTestBeat;
+    [SerializeField] float _pulseSize = 1.15f;
+    [SerializeField] float _returnSpeed = 5f;
+    private Vector3 _startSize;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _startSize = transform.localScale;
+        if (_useTestBeat)
+        {
+            StartCoroutine(TestBeat());
+        }
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        transform.localScale = Vector3.Lerp(transform.localScale, _startSize, Time.deltaTime * _returnSpeed);
+    }
+
+    public void Pulse()
+    {
+        transform.localScale = _startSize * _pulseSize;
+    }
+
+    IEnumerator TestBeat()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1f);
+            Pulse();
+        }
+    }
+}
---
+++ MechMania/Assets/Scripts/SceneChange.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//https://www.youtube.com/watch?v=mAiQJoblHh4
+
+public class SceneChange : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        pass;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pass;
+    }
+}
