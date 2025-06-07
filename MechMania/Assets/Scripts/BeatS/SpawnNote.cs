4b2f1befd98fc7e368376201d309aa0de8353335
Raquel Fellman <115256137+Seiami@users.noreply.github.com>
2025-06-07 00:04:29 -0500

    Attempt 2 on button fix

--- MechMania/Assets/Scripts/BeatS/SpawnNote.cs
+++ MechMania/Assets/Scripts/BeatS/SpawnNote.cs
@@ -15,13 +15,14 @@ public class SpawnNote : MonoBehaviour
     public void Spawn()
     {                                                                //Quaternion.identity
         GameObject note = Instantiate(notePrefab, spawnPoint.position, notePrefab.transform.rotation); //Spawns the note at the spawn point (quaternion.identity means no rotation)
+        AddNoteToXRButton(note.GetComponent<NoteObject>()); //Adds the note's OnButtonPress to the selectEntered event of the XR Interactable
         //note.AddComponent<Rigidbody>(); //Adds a rigidbody to the note
         note.transform.SetParent(spawnPoint);
 
     }
 
     /// Adds the note's OnButtonPress to the selectEntered event of the given XR Interactable GameObject.
-    public static void AddNoteToXRButton(NoteObject note)
+    public void AddNoteToXRButton(NoteObject note)
     {
         ButtonType[] buttons = FindObjectsOfType<ButtonType>();
         //var interactable = xrButtonObject.GetComponent<XRSimpleInteractable>();
@@ -30,6 +31,7 @@ public class SpawnNote : MonoBehaviour
             foreach (ButtonType button in buttons){
                 if (button.type == note.type)
                 {
+                    Debug.Log("Found button of type: " + note.type); //Debug log to check if the button was found
                     GameObject xrButtonObject = button.gameObject; //Get the GameObject of the button
                     XRSimpleInteractable xrInteractable = xrButtonObject.GetComponent<XRSimpleInteractable>(); //Get the XR Interactable component from the button GameObject
                     xrInteractable.selectEntered.AddListener((args) => note.OnButtonPress()); //Adds the note's OnButtonPress to the selectEntered event of the XR Interactable         
